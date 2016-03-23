abf363c6a12a78cd0bd2d582c7e13ba6b7088a2e
lfsobrinho <luisfelipee.sob@gmail.com>
2016-03-22 21:06:13 -0300

    sasa

---
+++ src/application/TelaPrincipalController.java
@@ -0,0 +1,20 @@
+package application;
+
+public class TelaPrincipalController {
+	package application;
+
+	import javafx.fxml.FXML;
+	import javafx.scene.control.Button;
+
+	public class TelaPrincipalController {
+
+	    @FXML
+	    private Button botao;
+
+	    @FXML
+	    void executar(ActionEvent event) {
+	    	System.out.println(campo.getText());
+	    }
+
+	}
+
